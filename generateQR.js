5e00027b016de8fc33c06718bf9ff335bf47cfd5
Neeraj <neerajcoder1@users.noreply.github.com>
2026-04-04 19:40:33 +0530

    Move QR asset to public and update QR output path

--- generateQR.js
+++ generateQR.js
@@ -1,7 +1,7 @@
 const QRCode = require("qrcode");
 
 QRCode.toFile(
-  "student-qr.png",
+  "public/student-qr.png",
   "https://food-court-app-chi.vercel.app/",
   { width: 300 },
   function (err) {
