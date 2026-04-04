375a626d1f5b54360ba686faf762013c15e7d1aa
Neeraj <neerajcoder1@users.noreply.github.com>
2026-04-04 20:18:03 +0530

    Add vendor login QR code

--- generateQR.js
+++ generateQR.js
@@ -1,11 +1,24 @@
 const QRCode = require("qrcode");
 
-QRCode.toFile(
-  "public/student-qr.png",
-  "https://food-court-app-chi.vercel.app/",
-  { width: 300 },
-  function (err) {
-    if (err) throw err;
-    console.log("QR saved");
+const qrTargets = [
+  {
+    file: "public/student-qr.png",
+    url: "https://food-court-app-chi.vercel.app/",
   },
-);
+  {
+    file: "public/vendor-qr.png",
+    url: "https://food-court-app-chi.vercel.app/vendor-login.html",
+  },
+];
+
+Promise.all(
+  qrTargets.map((target) =>
+    QRCode.toFile(target.file, target.url, { width: 300 }),
+  ),
+)
+  .then(() => {
+    console.log("QR codes saved");
+  })
+  .catch((err) => {
+    throw err;
+  });
